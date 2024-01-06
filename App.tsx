35eaba1b835737f23d2511f076450a3f8eec4c76
Dipanshu Sharma <dipanshu.sharma0610@gmail.com>
2024-01-06 22:17:24 +0530

    BMI calculator app

--- App.tsx
+++ App.tsx
@@ -1,118 +1,129 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { Image, StyleSheet, Text, TextInput, TouchableHighlight, View } from 'react-native'
+import React, { useState } from 'react'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const [height,setHeight]=useState('');
+  const [weight,setWeight]=useState('');
+  let [result, setResult]=useState(0);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  let reset=()=>{
+    setHeight('');
+    setWeight('');
+    setResult(0);
+  }
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  let calculateBMI=()=>{
+    let Result=Number(weight)/((Number(height)/100)*(Number(height)/100));
+    setResult(Result);
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <View style={styles.container}>
+      <Image source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWQwNPwVp58OnIA5N51_RDmAKnkOXe2ZgkDg&usqp=CAU'}}
+      style={styles.image}></Image>
+      <View style={styles.body}>
+        <View>
+          <Text style={styles.label}>Enter height(in Cms)</Text>
+          <TextInput
+            value={height}
+            onChangeText={setHeight}
+            style={styles.inputBox}
+            keyboardType='numeric'
+          />
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+        <View>
+          <Text style={styles.label}>Enter weight(in Kgs)</Text>
+          <TextInput
+            value={weight}
+            onChangeText={setWeight}
+            style={styles.inputBox}
+            keyboardType='numeric'
+          />
+        </View>
+
+        {result!=0?<View>
+            <Text style={styles.result}>
+              Result : {result}
+            </Text>
+          </View>:null
+        }
+        {result<18.5&&result!=0?<Text style={styles.resultText}>you are under weight !! put on some weight</Text>:null}
+        {result>=18.5&&result<25?<Text style={styles.resultText}>you are healthy</Text>:null}
+        {result>25?<Text style={styles.resultText}>you are over weight !! loose some weight</Text>:null}
+        
+        <TouchableHighlight 
+          activeOpacity={0.8}
+          underlayColor={'#EAF0F1'}
+          style={styles.ButtonContainer}
+          onPress={calculateBMI}
+        >
+          <View style={styles.myButton}>
+            <Text style={{color:'white', fontSize: 16}}>Check my BMI</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight 
+          activeOpacity={0.8}
+          underlayColor={'#EAF0F1'}
+          style={styles.ButtonContainer}
+          onPress={reset}
+        >
+          <View style={styles.myButton}>
+            <Text style={{color:'white', fontSize: 16}}>Reset</Text>
+          </View>
+        </TouchableHighlight>
+        
+    </View>
+      
+    </View>
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container:{
+    flex:1
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  body:{
+    padding:12,
+    flex:1,
+    justifyContent:'flex-start'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  image:{
+    height:100,
+    width:'100%'
   },
-  highlight: {
-    fontWeight: '700',
+  inputBox:{
+    borderColor: '#2475B0',
+    borderWidth:2,
+    borderRadius: 10,
+    marginBottom: 16
   },
-});
-
-export default App;
+  label:{
+    color:'#000000',
+    fontSize: 18
+  },
+  ButtonContainer:{
+    marginBottom:10,
+    marginTop: 10,
+    paddingVertical:12,
+    backgroundColor: '#1287A5',
+    borderRadius: 8
+  },
+  myButton:{
+    alignItems: 'center',
+  },
+  resultText:{
+    color: '#2F363F',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  result:{
+    color:'black', 
+    fontSize:16, 
+    fontWeight:'bold',
+    textAlign: 'center'
+  }
+})
